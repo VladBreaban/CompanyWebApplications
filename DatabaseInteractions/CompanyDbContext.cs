bebb1a29bdf3b921f025b8400657e269897b982a
VladBreaban <vladbreaban@yahoo.com>
2022-11-11 18:53:59 +0200

    Add project files.

---
+++ DatabaseInteractions/CompanyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using DatabaseInteractions.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseInteractions
+{
+    public class CompanyDbContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public CompanyDbContext() : base() { }
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options) { }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            var configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+
+    }
+}
---
+++ DatabaseInteractions/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseInteractions.Models
+{
+    [Table("Companies")]
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Exchange { get; set; }
+        public string Ticker { get; set; }
+        public string Isin { get; set; }
+        public string Website { get; set; }
+    }
+}
