4bae7ad588b5f7947a7bc727abf68d7024684256
VladBreaban <vladbreaban@yahoo.com>
2022-11-13 13:02:03 +0200

    added snackbar

--- CompanyWebApplications/Controllers/CompanyController.cs
+++ CompanyWebApplications/Controllers/CompanyController.cs
@@ -7,65 +7,82 @@ using Microsoft.IdentityModel.Tokens;
 using System.Net.WebSockets;
 namespace CompanyWebApplications.Controllers;
 
-    [ApiController]
-    [Route("[controller]")]
-    [Authorize]
-    public class CompanyController : ControllerBase
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class CompanyController : ControllerBase
+{
+
+    private readonly ILogger<CompanyController> _logger;
+    private readonly ICompanyService _companyService;
+    public CompanyController(ILogger<CompanyController> logger, ICompanyService companyService)
+    {
+        _logger = logger;
+        _companyService = companyService;
+    }
+
+    [HttpGet("GetCompanyById/{companyId}")]
+    public async Task<ActionResult<CompanyApiModel>> GetCompanyById(Guid companyId)
+    {
+        if (companyId == Guid.Empty)
+        {
+            return BadRequest("Received empty id");
+        }
+
+        return await _companyService.GetById(companyId.ToString());
+    }
+
+
+    [HttpGet("GetCompanyByIsin/{companyIsin}")]
+    public async Task<ActionResult<CompanyApiModel>> GetCompanyByIsin(string companyIsin)
+    {
+        if (companyIsin.IsNullOrEmpty())
+        {
+            return BadRequest("Received empty isin");
+        }
+
+        return await _companyService.GetByIsin(companyIsin);
+    }
+
+    [HttpGet("GetAllCompanies")]
+    public async Task<ActionResult<List<CompanyApiModel>>> GetAllCompanies()
+    {
+        return await _companyService.GetAll();
+    }
+
+    [HttpPost("UpdateCompany")]
+    public async Task<ActionResult<CompanyApiModel>> UpdateCompany([FromBody] CompanyApiModel company)
+    {
+        if (company == null)
+        {
+            return BadRequest("Company should not be null");
+        }
+        try
+        {
+            var result = await _companyService.UpdateCompanyById(company.Id, company);
+            return Ok(company);
+        }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+    }
+
+    [HttpPost("CreateCompany")]
+    public async Task<ActionResult<CompanyApiModel>> CreateCompany([FromBody] CompanyApiModel company)
     {
 
-        private readonly ILogger<CompanyController> _logger;
-        private readonly ICompanyService _companyService;
-        public CompanyController(ILogger<CompanyController> logger, ICompanyService companyService)
+        try
         {
-            _logger = logger;
-            _companyService = companyService;
-        }
-
-        [HttpGet("GetCompanyById/{companyId}")]
-        public async Task<ActionResult<CompanyApiModel>> GetCompanyById(Guid companyId)
-        {
-            if (companyId == Guid.Empty)
-            {
-                return BadRequest("Received empty id");
-            }
-
-            return await _companyService.GetById(companyId.ToString());
-        }
-     
-
-        [HttpGet("GetCompanyByIsin/{companyIsin}")]
-        public async Task<ActionResult<CompanyApiModel>> GetCompanyByIsin(string companyIsin)
-        {
-            if (companyIsin.IsNullOrEmpty())
-            {
-                return BadRequest("Received empty isin");
-            }
-
-            return await _companyService.GetByIsin(companyIsin);
-        }
-
-        [HttpGet("GetAllCompanies")]
-        public async Task<ActionResult<List<CompanyApiModel>>> GetAllCompanies()
-        {
-            return await _companyService.GetAll();
-        }
-
-        [HttpPost("UpdateCompany")]
-        public async Task<ActionResult<CompanyApiModel>> UpdateCompany([FromBody] CompanyApiModel company)
-        {
-           if(company == null)
-            {
-                return BadRequest();
-            }          
-            var result = await _companyService.UpdateCompanyById(company.Id, company);
-            return result==true? Ok(company) : BadRequest("Could not update entity");
-        }
-
-        [HttpPost("CreateCompany")]
-        public async Task<ActionResult<CompanyApiModel>> CreateCompany([FromBody] CompanyApiModel company)
-        {
-
             var result = await _companyService.AddCompany(company);
-            return result != Guid.Empty ? Ok(company) : BadRequest("Could not create entity");
+            return Ok(company);
         }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+
     }
+}
--- DatabaseInteractions/Services/CompanyService.cs
+++ DatabaseInteractions/Services/CompanyService.cs
@@ -5,21 +5,65 @@ using DatabaseInteractions.ServicesInterfaces;
 
 namespace DatabaseInteractions.Services;
 
-    public class CompanyService : ICompanyService
+public class CompanyService : ICompanyService
+{
+    private readonly ICompanyRepository _companyRepository;
+    public CompanyService(ICompanyRepository companyRepository)
     {
-        private readonly ICompanyRepository _companyRepository;
-        public CompanyService(ICompanyRepository companyRepository)
+        _companyRepository = companyRepository;
+    }
+    public async Task<CompanyApiModel> GetById(string companyId)
+    {
+        CompanyApiModel result = null;
+        // could use a mapper here
+        var companyEntity = await _companyRepository.GetById(companyId);
+        if (companyEntity is not null)
         {
-            _companyRepository = companyRepository;
-        }
-        public async Task<CompanyApiModel> GetById(string companyId)
-        {
-            CompanyApiModel result = null;
-            // could use a mapper here
-            var companyEntity = await _companyRepository.GetById(companyId);
-            if(companyEntity is not null )
+            result = new CompanyApiModel
             {
-                result = new CompanyApiModel
+                Id = companyEntity.Id,
+                Name = companyEntity.Name,
+                Exchange = companyEntity.Exchange,
+                Ticker = companyEntity.Ticker,
+                Isin = companyEntity.Isin,
+                Website = companyEntity.Website
+            };
+        }
+
+        return result;
+    }
+
+    public async Task<CompanyApiModel> GetByIsin(string companyIsin)
+    {
+        CompanyApiModel result = null;
+        // could use a mapper here
+        var companyEntity = await _companyRepository.GetByIsin(companyIsin);
+        if (companyEntity is not null)
+        {
+            result = new CompanyApiModel
+            {
+                Id = companyEntity.Id,
+                Name = companyEntity.Name,
+                Exchange = companyEntity.Exchange,
+                Ticker = companyEntity.Ticker,
+                Isin = companyEntity.Isin,
+                Website = companyEntity.Website
+            };
+        }
+
+        return result;
+    }
+
+    public async Task<List<CompanyApiModel>> GetAll()
+    {
+        List<CompanyApiModel> result = new List<CompanyApiModel>();
+        // could use a mapper here
+        var companies = await _companyRepository.GetAll();
+        if (companies.Count != 0)
+        {
+            foreach (var companyEntity in companies)
+            {
+                var company = new CompanyApiModel
                 {
                     Id = companyEntity.Id,
                     Name = companyEntity.Name,
@@ -28,110 +72,66 @@ namespace DatabaseInteractions.Services;
                     Isin = companyEntity.Isin,
                     Website = companyEntity.Website
                 };
+                result.Add(company);
             }
-
-            return result;
         }
-
-        public async Task<CompanyApiModel> GetByIsin(string companyIsin)
+        return result;
+    }
+    public async Task<bool> UpdateCompanyById(Guid companyId, CompanyApiModel company)
+    {
+        try
         {
-            CompanyApiModel result = null;
-            // could use a mapper here
-            var companyEntity = await _companyRepository.GetByIsin(companyIsin);
-            if (companyEntity is not null)
+            if (company != null)
             {
-                result = new CompanyApiModel
+                var companyEnitity = await _companyRepository.GetById(companyId.ToString());
+
+                if (companyEnitity != null)
                 {
-                    Id = companyEntity.Id,
-                    Name = companyEntity.Name,
-                    Exchange = companyEntity.Exchange,
-                    Ticker = companyEntity.Ticker,
-                    Isin = companyEntity.Isin,
-                    Website = companyEntity.Website
-                };
-            }
-
-            return result;
-        }
-
-        public async Task<List<CompanyApiModel>> GetAll()
-        {
-            List<CompanyApiModel> result = new List<CompanyApiModel>();
-            // could use a mapper here
-            var companies = await _companyRepository.GetAll();
-            if (companies.Count != 0)
-            {
-                foreach (var companyEntity in companies)
-                {
-                    var company = new CompanyApiModel
-                    {
-                        Id = companyEntity.Id,
-                        Name = companyEntity.Name,
-                        Exchange = companyEntity.Exchange,
-                        Ticker = companyEntity.Ticker,
-                        Isin = companyEntity.Isin,
-                        Website = companyEntity.Website
-                    };
-                    result.Add(company);
-                }              
-            }
-            return result;
-        }
-        public async Task<bool> UpdateCompanyById(Guid companyId, CompanyApiModel company)
-        {
-            try
-            {
-                if (company != null)
-                {
-                    var companyEnitity = await _companyRepository.GetById(companyId.ToString());
-
-                    if(companyEnitity != null)
-                    {
-                        companyEnitity.Id = companyId;
-                        companyEnitity.Isin = company.Isin;
-                        companyEnitity.Exchange = company.Exchange;
-                        companyEnitity.Ticker = company.Ticker;
-                        companyEnitity.Website = company.Website;
-                        companyEnitity.Name = company.Name;
-
-                        await _companyRepository.UpdateEntity(companyEnitity).ConfigureAwait(false);
-                    }                   
-                }
-            }
-            catch(Exception ex)
-            {
-                //would be a great idea to have a logger here
-                return false;
-            }
-
-            return true;
-        }
-        public async Task<Guid> AddCompany(CompanyApiModel company)
-        {
-            try
-            {
-                if (company != null)
-                {
-                    Company companyEnitity = new Company();
-                    companyEnitity.Id = Guid.NewGuid();
+                    companyEnitity.Id = companyId;
                     companyEnitity.Isin = company.Isin;
                     companyEnitity.Exchange = company.Exchange;
                     companyEnitity.Ticker = company.Ticker;
                     companyEnitity.Website = company.Website;
                     companyEnitity.Name = company.Name;
 
-                   await _companyRepository.CreateEntity(companyEnitity).ConfigureAwait(false);
-                   return companyEnitity.Id;
-                }  
+                    await _companyRepository.UpdateEntity(companyEnitity).ConfigureAwait(false);
+                }
             }
-            catch (Exception ex)
-            {
-                //would be a great idea to have a logger here
-                return Guid.Empty;
-            }
-
-            return Guid.Empty;
-
         }
+        catch (Exception ex)
+        {
+            //would be a great idea to have a logger here
+            throw;
+        }
+
+        return true;
     }
+    public async Task<Guid> AddCompany(CompanyApiModel company)
+    {
+        try
+        {
+            if (company != null)
+            {
+                Company companyEnitity = new Company();
+                companyEnitity.Id = Guid.NewGuid();
+                companyEnitity.Isin = company.Isin;
+                companyEnitity.Exchange = company.Exchange;
+                companyEnitity.Ticker = company.Ticker;
+                companyEnitity.Website = company.Website;
+                companyEnitity.Name = company.Name;
+
+                await _companyRepository.CreateEntity(companyEnitity).ConfigureAwait(false);
+                return companyEnitity.Id;
+            }
+        }
+        catch (Exception ex)
+        {
+            //would be a great idea to have a logger here
+            throw;
+        }
+
+        return Guid.Empty;
+
+    }
+}
 
